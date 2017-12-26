e04a35f6cdc84ab7bc6b16cecaae3892f60c782a
Armen Baghumian <armen@vardump.org>
2017-12-26 11:21:03 +1100

    Add an example for async job handler

--- job/handler/async/async_test.go
+++ job/handler/async/async_test.go
@@ -1,8 +1,10 @@
 package async_test
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/armen/dp/job"
 	"github.com/armen/dp/job/handler/async"
 	"github.com/armen/dp/job/internal/test"
 )
@@ -15,4 +17,27 @@ func TestGuaranteedResponse(t *testing.T) {
 func TestProcess(t *testing.T) {
 	jh := async.New()
 	test.Process(jh, t)
+}
+
+func Example() {
+	var msg = make(chan string)
+
+	jh := async.New()
+	jh.Confirm(func(j job.Job) {
+		msg <- fmt.Sprintln("Confirmed", j)
+	})
+	go jh.React()
+
+	jh.Submit("Job 1")
+	jh.Submit("Job 2")
+	jh.Submit("Job 3")
+
+	fmt.Print(<-msg)
+	fmt.Print(<-msg)
+	fmt.Print(<-msg)
+
+	// Unordered output:
+	// Confirmed Job 1
+	// Confirmed Job 2
+	// Confirmed Job 3
 }
