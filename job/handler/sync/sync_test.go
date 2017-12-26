f878ff1cb7fec5912476f2e5694bc6a1b44e067c
Armen Baghumian <armen@vardump.org>
2017-12-26 11:21:28 +1100

    Add an example for sync job handler

--- job/handler/sync/sync_test.go
+++ job/handler/sync/sync_test.go
@@ -1,18 +1,43 @@
 package sync_test
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/armen/dp/job"
+	jh "github.com/armen/dp/job/handler/sync"
 	"github.com/armen/dp/job/internal/test"
-	"github.com/armen/dp/job/handler/sync"
 )
 
 func TestGuaranteedResponse(t *testing.T) {
-	jh := sync.New()
+	jh := jh.New()
 	test.GuaranteedResponse(jh, t)
 }
 
 func TestProcess(t *testing.T) {
-	jh := sync.New()
+	jh := jh.New()
 	test.Process(jh, t)
+}
+
+func Example() {
+	var msg = make(chan string)
+
+	jh := jh.New()
+	jh.Confirm(func(j job.Job) {
+		msg <- fmt.Sprintln("Confirmed", j)
+	})
+	go jh.React()
+
+	jh.Submit("Job 1")
+	jh.Submit("Job 2")
+	jh.Submit("Job 3")
+
+	fmt.Print(<-msg)
+	fmt.Print(<-msg)
+	fmt.Print(<-msg)
+
+	// Unordered output:
+	// Confirmed Job 1
+	// Confirmed Job 2
+	// Confirmed Job 3
 }
